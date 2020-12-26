b00ceda43e6cf11d8f42171a8a98716072baf24d
배진환 <76583684+bjh3311@users.noreply.github.com>
2020-12-26 17:45:51 +0900

    백준 2309번 brute force로 문제해결

---
+++ 백준 2309번.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2309번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[9];
+            int[] result = new int[7];
+            int sum=0;
+            for(int i=0;i<9;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());                
+            }
+            for(int i=0;i<9;i++)
+            {
+                sum += arr[i];
+            }
+            for(int i=0;i<9;i++)
+            {
+                for(int j=i+1;j<9;j++)
+                {
+                    if(sum-arr[i]-arr[j]==100)
+                    {
+                        for(int a=0;a<i;a++)
+                        {
+                            result[a] = arr[a];
+                        }
+                        for(int a=i+1;a<j;a++)
+                        {
+                            result[a-1] = arr[a];
+                        }
+                        for(int a=j+1;a<9;a++)
+                        {
+                            result[a - 2] = arr[a];
+                        }
+                    }
+                }
+            }
+            Array.Sort(result);
+            foreach(int item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
