d570bb2ee21a545a018011fff1b3823b76e1ca92
배진환 <76583684+bjh3311@users.noreply.github.com>
2020-12-26 16:20:00 +0900

    백준 11729번 stringbuilder로 시간초과해결

---
+++ 백준 11729번.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static System.Text.StringBuilder sb = new System.Text.StringBuilder();
+    static void Hanoi(int n, int a, int b, int c)
+    {
+        if (n == 1)
+        {
+            sb.AppendFormat("{0} {1}\n", a, c);
+            return;
+       
+        }
+        Hanoi(n - 1, a, c, b);
+        Hanoi(1, a, b, c);
+        Hanoi(n - 1, b, a, c);
+    }
+    static void Main()
+
+    {
+
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine((int)Math.Pow(2, n) - 1);
+
+        Hanoi(n, 1, 2, 3);
+
+        Console.Write(sb);
+
+    }
+
+}
