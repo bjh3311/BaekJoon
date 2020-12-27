404a4e3ba3322232918ecd376d32d65a289afd26
배진환 <76583684+bjh3311@users.noreply.github.com>
2020-12-27 17:54:48 +0900

    백준 10171번 고양이출력

---
+++ 백준 10171번.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_10171번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
