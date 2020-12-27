ddf504a15cf531f6ea93728a178b1f41bef11a45
배진환 <76583684+bjh3311@users.noreply.github.com>
2020-12-27 18:03:00 +0900

    백준 10430번 단순 계산 문제

---
+++ 백준 10430.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_10430번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int A = int.Parse(s[0]);
+            int B = int.Parse(s[1]);
+            int C = int.Parse(s[2]);
+            Console.WriteLine("{0}",(A+B)%C);
+            Console.WriteLine("{0}", ((A % C) + (B % C)) % C);
+            Console.WriteLine("{0}", (A*B) % C);
+            Console.WriteLine("{0}", ((A % C)* (B % C)) % C);
+
+
+        }
+    }
+}
