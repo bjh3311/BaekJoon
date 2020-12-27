4bbde89aa178d807b9a0e6725182c2c5f4f1d295
배진환 <76583684+bjh3311@users.noreply.github.com>
2020-12-27 18:15:51 +0900

    백준 1065번 brute force 사용

---
+++ 백준 1065번.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1065번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int count = 0;
+            if(100<=N && N<110)
+            {
+                Console.WriteLine(99);
+            }
+            else if(N<100)
+            {
+                Console.WriteLine(N);
+            }
+            else
+            {
+                for (int i = 111; i <= N; i++)
+                {
+                    int a = i / 100;
+                    int b = (i / 10) % 10;
+                    int c = i % 10;
+                    int d = b - a;
+                    if(c==(a+2*d))
+                    {
+                        count++;
+                    }
+                        }
+                Console.WriteLine(count + 99);
+            }
+           
+        }
+    }
+}
