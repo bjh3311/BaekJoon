381934998a82c5a322ea9dfabf9d2a3d403788e3
배진환 <76583684+bjh3311@users.noreply.github.com>
2020-12-28 22:45:11 +0900

    백준 1436번 brute force문제이다. 숫자를 문자열로 바꿔줘서 문제 해결

---
+++ 백준 1436번.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int count = 0;
+        for (int i = 666; ; i++)
+        {
+            for (int j = 0; j < i.ToString().Length - 2; j++)
+            {
+                if (i.ToString()[j] == '6')
+                {
+                    if (i.ToString()[j + 1] == '6')
+                    {
+                        if (i.ToString()[j + 2] == '6')
+                        {
+                            count++;
+                            break;
+                        }
+                    }
+                }
+            }
+            if (count == N)
+            {
+                Console.WriteLine(i);
+                break;
+            }
+        }
+    }
+}
