4d3899066260a458da2290a9e4ed64d40a40c5f0
배진환 <76583684+bjh3311@users.noreply.github.com>
2020-12-29 23:33:54 +0900

    백준 3052번

---
+++ 백준 3052번.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_3052번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int count = 0;
+            for(int i=0;i<10;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                arr[i] = arr[i] % 42;
+            }
+            Array.Sort(arr);
+            for (int i = 0; i < 9; i++)
+            {
+                if(arr[i]!=arr[i+1])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count+1);
+
+        }
+    }
+}
