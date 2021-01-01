c12fccf88d0f0fee0afd0e03697a86ad9e304c07
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-01 21:16:44 +0900

    백준 2675번. 문자열 반복 문제

---
+++ 백준 2675번.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1157번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t_num = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t_num; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                for (int k = 0; k < s[1].Length; k++)                
+                {
+                     for (int j = 0; j < int.Parse(s[0]); j++)
+                     {
+                        Console.Write(s[1].ToString()[k]);
+                     }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
