022c20fe1c77d3173ac95ae1e8ab07d6a1c41217
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-01 20:41:33 +0900

    백준 10809번. 문자열과 아스키 코드를 활용한 문제

---
+++ 백준 10809번.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_10809번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int Length = s.Length;
+            int[] arr = new int[26];
+            for (int i = 0; i < 26; i++)
+            {
+                arr[i] = -1;
+            }
+            for (int i=0;i<Length;i++)
+            {
+                int a=Convert.ToInt32(s.ToString()[i]);
+                if (arr[a - 97] == -1)
+                {
+                    arr[a - 97] = i;
+                }
+            }
+
+            for(int i=0;i<26;i++)
+            {
+                Console.Write("{0} ", arr[i]);
+            }
+
+
+        }
+    }
+}
