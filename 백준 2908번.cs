c274a8a437195c7b54f8bdc3e4b4ae58080827f5
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-02 20:06:34 +0900

    백준 2908번. 문자열 뒤집기 문제

---
+++ 백준 2908번.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2908번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int A = int.Parse(s[0]);
+            int B = int.Parse(s[1]);
+            int A_1 = A % 10;
+            int A_2 = (A / 10) % 10;
+            int A_3 = A / 100;
+            int B_1 = B % 10;
+            int B_2 = (B / 10) % 10;
+            int B_3 = B / 100;
+            int renewal_A = (A_1)*100+(A_2)*10+A_3;
+            int renewal_B = (B_1) * 100 + (B_2) * 10 + B_3;
+            if(renewal_A>renewal_B)
+            {
+                Console.Write("{0}", renewal_A);
+            }
+            else
+            {
+                Console.Write("{0}", renewal_B);
+            }
+        } 
+    }
+}
