29b2f7868aa8556ccbf4d6eab697fecccc25bc46
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-03 19:26:02 +0900

    백준 1152번. 문자열에서 공백제거하여 문제해결

---
+++ 백준 1152번.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1152번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string s_trim = s.Trim();
+            int before = s_trim.Length;
+            string s_replace = s_trim.Replace(" ", "");
+            int after = s_replace.Length;
+            if(before-after==0)
+            {
+                if(before>0)
+                {
+                    Console.WriteLine(1);
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+            else
+            {
+                Console.WriteLine(before - after + 1);
+            }
+            
+        }
+    }
+}
