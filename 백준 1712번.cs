1af19b4cfa3df9d4e7e636bbf4aaac3cba82bfe4
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-04 23:04:06 +0900

    백준 1712번. 정답률이 23퍼인게 의아할정도의 간단한 수학문제

---
+++ 백준 1712번.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1712번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int A = int.Parse(s[0]);
+            int B = int.Parse(s[1]);
+            int C = int.Parse(s[2]);
+            int i;
+            if(B>=C)
+            {
+                Console.WriteLine(-1);
+                goto Exit;
+
+            }
+            int x = A / (C - B);
+            Console.WriteLine(x + 1);
+            Exit:
+            {
+
+            }
+        }
+    }
+}
