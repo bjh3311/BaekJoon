94533ee2d097a9b01cefad0e67b2b53a18da93bf
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-04 23:12:30 +0900

    백준 2869번. 문제는 간단하지만 시간제한0.15초를 못지켜서 시간초과로 틀림. 추후 수정 예정

---
+++ 백준 2869번.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2869번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int A = int.Parse(s[0]);
+            int B = int.Parse(s[1]);
+            int V = int.Parse(s[2]);
+            int count = 0;
+            while(true)
+            {
+                if(V-A>0)
+                {
+                    V = V - A + B;
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(count+1);
+                
+        }
+    }
+}
