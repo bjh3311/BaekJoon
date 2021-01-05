346c4ea15c3bb6fae46bace05f3a33cdd95e0dbf
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-05 23:56:08 +0900

    백준 1978번. 소수찾기문제

---
+++ 백준 1978번.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1978번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split();
+            int count = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 2; j <= int.Parse(s[i]); j++)
+                {
+                    if (int.Parse(s[i]) == 2)
+                    {
+                        count++;
+                        break;
+                    }
+                    else if (int.Parse(s[i]) % j == 0)
+                    {
+
+                        break;
+                    }
+                    else if (j + 1 == int.Parse(s[i]))
+                    {
+                        count++;
+                        break;
+                    }
+
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
