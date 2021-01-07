df256451ec59c917501fee8ebf6df6a36caa7fa5
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-08 01:37:10 +0900

    백준 1929번. 에라토스테네스의 체를 이용하여 문제 해결

---
+++ 백준 1929번.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1929번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int M = int.Parse(s[0]);
+            int N = int.Parse(s[1]);
+            int[] arr = new int[1000001];
+            StringBuilder result = new StringBuilder();
+            for(int i=0;i<1000001;i++)
+            {
+                arr[i] = 0;
+            }//배열 초기화
+            for(int i=2;i<=1000000;i++)
+            {
+                if(arr[i]==0)
+                {
+                    for(int j=2;j*i<=1000000;j++)
+                    {
+                        arr[j * i] = 1;
+                    }
+                }
+                
+            }//소수가 아니면 1을 넣는다
+            arr[1] = 1;
+            for(int i=M;i<=N;i++)
+            {
+                if(arr[i]==0)
+                {
+                    result.Append(i);
+                    result.Append("\n");
+                }
+                
+            }
+            Console.Write(result.ToString());
+        }
+    }
+}
