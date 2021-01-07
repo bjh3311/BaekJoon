c27a5763a466138d109f56af9437fca5db7fe29b
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-07 21:38:13 +0900

    백준 2292번. 벌집찾기 문제. 패턴만 알면 간단하다

---
+++ 백준 2292번.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2292번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int data = 7;
+            int i;
+           if(N==1)
+            {
+                Console.WriteLine(1);
+            }
+           else
+            {
+                for(i=2;data<N;i++)
+                {
+                    data = data + (6 * i);
+                }
+                Console.WriteLine(i);             
+            }
+
+        }
+    }
+}
