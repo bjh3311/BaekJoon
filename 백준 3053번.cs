245df7dc1de0c99c3c13fef8cda3dc019fa51e48
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-07 22:23:35 +0900

    백준 3053번. 정답률이 40%를 못 넘는게 신기한 문제

---
+++ 백준 3053번.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace 백준_3053번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double R = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f6}",(Math.PI) * R*R);
+            Console.WriteLine(R * R * 2);
+        }
+    }
+}
