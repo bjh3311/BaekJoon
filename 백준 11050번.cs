913b7f488e7b5bd3023e2eadcd1e2d755447b7a5
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-09 22:45:02 +0900

    백준 11050번. 이항계수(조합C) 구현하는 간단한 문제

---
+++ 백준 11050번.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_11050번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int N = int.Parse(s[0]);
+            int K = int.Parse(s[1]);
+            int result = 1;
+            for(int i=N;i>N-K;i--)
+            {
+                result *= i;
+            }
+            for(int i=K;i>0;i--)
+            {
+                result /= i;
+            }
+            Console.Write(result);
+        }
+    }
+}
