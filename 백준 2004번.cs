3e18073e1dd0956268d2b7a379b55d9425fb875d
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-09 23:29:42 +0900

    백준 2004번. 팩토리얼의 0의개수 찾는문제랑 비슷하게 풀었지만 시간초과로 실패
    팩토리얼은 입력의 범위가 500까지지만 이 문제는 20억까지여서 시간초과를 해결할 방법을 생각해야한다.

---
+++ 백준 2004번.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2004번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int two_count = 0;
+            int five_count = 0;
+            int zero_count = 0;
+            string[] s = Console.ReadLine().Split();
+            int n = int.Parse(s[0]);
+            int m = int.Parse(s[1]);
+            for (int i = n; i > n - m; i--)
+            {
+                int temp = i;
+                for(int j=2;j<=i;)
+                {
+                    if(temp%j==0)
+                    {
+                        if(j==2)
+                        {
+                            two_count++;
+                        }
+                        else if(j==5)
+                        {
+                            five_count++; 
+                        }
+                        temp = temp / j;
+                        if(temp==1)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+            }
+            for (int i = m; i > 0; i--)
+            {
+                int temp = i;
+                for (int j = 2; j <= i;)
+                {
+                    if (temp % j == 0)
+                    {
+                        if (j == 2)
+                        {
+                            two_count--;
+                        }
+                        else if (j == 5)
+                        {
+                            five_count--;
+                        }
+                        temp = temp / j;
+                        if (temp == 1)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        j++;
+                    }
+                }
+            }
+            while(true)
+            {
+                if(two_count!=0&&five_count!=0)
+                {
+                    two_count--;
+                    five_count--;
+                    zero_count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.Write(zero_count);
+
+        }
+    }
+}
