b2f6e4f2255e89c6775d082d2ca391612f668366
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-10 00:35:03 +0900

    백준 10093번. 간단한 숫자 출력문제

---
+++ 백준 10093번.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_11718번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            ulong A = ulong.Parse(s[0]);
+            ulong B = ulong.Parse(s[1]);
+            StringBuilder res = new StringBuilder(); 
+            if (A < B)
+            {
+                Console.WriteLine(B - A - 1);
+                for (ulong i = A+1; i < B; i++)
+                {
+
+                    res.Append(i);
+                    res.Append(" ");
+                }
+                Console.Write(res.ToString());
+            }
+            else if (A > B)
+            {
+                Console.WriteLine(A - B - 1);
+                for (ulong i = B+1; i < A; i++)
+                {
+
+                    res.Append(i);
+                    res.Append(" ");
+                }
+                Console.Write(res.ToString());
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+
+
+        }
+    }
+}
