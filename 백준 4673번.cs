cd271d4861502b43cd93f279a5accdebf3d54a47
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-09 20:30:11 +0900

    백준 4673번. 셀프넘버 문제이며 함수자체는 재귀가 아니지만 main에서 재귀로 구성

---
+++ 백준 4673번.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_4673번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10001];
+            StringBuilder result = new StringBuilder();
+            for(int i=0;i<10001;i++)
+            {
+                arr[i] = 0;
+            }
+            arr[0] = 1;
+            for(int i=1;i<10001;i++)
+            {
+                if(arr[i]==0)
+                {
+                    int j = i;
+                    result.Append(i);
+                    result.Append("\n");
+                    while(D(j)<=10000)
+                    {
+                        arr[D(j)] = 1;
+                        j = D(j);
+
+                    }
+                }
+            }   
+            Console.Write(result.ToString());    
+        }
+        private static int D(int n)
+        {
+            int sum = n;
+            while(n>0)
+            {
+                sum += n % 10;
+                n = n / 10;
+            }
+            return sum;
+        }
+    }
+}
