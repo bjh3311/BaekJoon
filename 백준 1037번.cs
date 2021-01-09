c7df2cc8b3f6708b6cf8f345daeeccddd142ccc2
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-09 22:36:40 +0900

    백준 1037번. 약수의 성질을 이용한 간단한 문제

---
+++ 백준 1037번.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1037번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            string[] s = Console.ReadLine().Split();
+            for(int i=0;i<N;i++)
+            {
+                arr[i] = int.Parse(s[i]);
+            }
+            int a = 1;
+            Array.Sort(arr);
+            Console.WriteLine(arr[0] * arr[N - 1]);
+        }
+    }
+}
