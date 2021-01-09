39c9b8c435ad95f140d1951388d3e7508b770d70
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-09 21:31:54 +0900

    백준 5086번. 간단한 배수, 약수 찾기 문제

---
+++ 백준 5086번.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_5086번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                int A, B;
+                string[] s = Console.ReadLine().Split();
+                A = int.Parse(s[0]);
+                B = int.Parse(s[1]);
+                if(A ==0&&B==0)
+                {
+                    break;
+                }
+                else
+                {
+                    if(A%B==0)
+                    {
+                        Console.WriteLine("multiple");
+                    }
+                    else if(B%A==0)
+                    {
+                        Console.WriteLine("factor");
+                    }
+                    else
+                    {
+                        Console.WriteLine("neither");
+                    }
+                }
+
+            }
+        }
+    }
+}
