60bb7d4bc7a60b0f34977fe8a7d22d48ac9ca065
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-10 16:32:53 +0900

    백준 2609번. 최소공배수와 최대공약수 찾기. 소인수분해를 응용하여 문제해결

---
+++ 백준 2609번.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2609번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int A = int.Parse(s[0]);
+            int B = int.Parse(s[1]);
+            int[] A_arr = new int[10000];
+            int[] A_arr2 = new int[10000];
+            int[] B_arr = new int[10000];
+            int[] B_arr2 = new int[10000];
+            int Max = 1;
+            int Min = 1;
+            for (int i=2;A!=1 ;)
+            {
+                if(A%i==0)
+                {
+                    A_arr[i]++;
+                    A_arr2[i]++;
+                    A = A / i;
+                }
+                else
+                {
+                    i++;
+                }
+
+            }
+            for (int i = 2; B!= 1;)
+            {
+                if (B% i == 0)
+                {
+                    B_arr[i]++;
+                    B_arr2[i]++;
+                    B = B / i;
+                }
+                else
+                {
+                    i++;
+                }
+
+            }
+            for(int i=2;i<10000;)//최대공약수 구하기
+            {
+                if(A_arr[i]!=0&&B_arr[i]!=0)
+                {
+                    Max = Max * i;
+                    A_arr[i]--;
+                    B_arr[i]--;
+                }
+                else if(A_arr[i]==0||B_arr[i]==0)
+                {
+                    i++;
+                }
+            }
+            for(int i=2;i<10000;)//최소공배수 구하기
+            {
+                if(A_arr2[i]>0||B_arr2[i]>0)
+                {
+                    Min = Min * i;
+                    A_arr2[i]--;
+                    B_arr2[i]--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            Console.WriteLine(Max);
+            Console.WriteLine(Min);
+
+        }
+    }
+}
