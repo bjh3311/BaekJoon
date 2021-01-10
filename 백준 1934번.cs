6eead34b8f3127b6e7fdf9eafc09961bec682c5e
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-10 17:04:10 +0900

    백준 1934번. 유클리드 알고리즘을 이용하여 문제해결

---
+++ 백준 1934번.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1934번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int i=0;i<T;i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                int A = int.Parse(s[0]);
+                int B = int.Parse(s[1]);
+                if(A%B==0)
+                {
+                    Console.WriteLine(A);
+                }
+                else if(B%A==0)
+                {
+                    Console.WriteLine(B);
+                }
+                else
+                {
+                    
+                    if(A>B)
+                    {
+                        int A_temp = A;
+                        int B_temp = B;
+                        while(true)
+                        {
+                            int C = A_temp % B_temp;
+                            if(C==0)
+                            {
+                                Console.WriteLine(A*(B/B_temp));
+                                break;
+                            }
+                            else
+                            {
+                                A_temp = B_temp;
+                                B_temp = C;
+                            }
+                        
+                        }
+                        
+                        
+                    }
+                    else
+                    {
+                        int A_temp = A;
+                        int B_temp = B;
+                        while(true)
+                        {
+                            int C = B_temp % A_temp;
+                            if(C==0)
+                            {
+                                Console.WriteLine(B * (A / A_temp));
+                                break;
+                            }
+                            else
+                            {
+                                B_temp = A_temp;
+                                A_temp = C;
+                            }
+                        }
+                       
+                    }
+                }
+            }
+        }
+    }
+}
