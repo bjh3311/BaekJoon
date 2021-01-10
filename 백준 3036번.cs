b03280cf1403b4455ee11fc117595e770059a6ca
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-10 18:35:11 +0900

    백준 3036번. 첫번째 링을 한 바퀴 돌렸을 때, 나머지 링이 몇 바퀴 도는지 구하는 문제

---
+++ 백준 3036번.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_3036번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            string[] s = Console.ReadLine().Split();
+            int[] arr = new int[N];
+            for(int i=0;i<N;i++)
+            {
+                arr[i] = int.Parse(s[i]);
+            }
+            for(int i=1;i<N;i++)
+            {
+                if(arr[0]%arr[i]==0)
+                {
+                    Console.WriteLine("{0}/{1}", arr[0] / arr[i], 1);
+                }
+                else
+                {
+
+                    if (arr[0]> arr[i])
+                    {
+                        int A_temp = arr[0];
+                        int B_temp = arr[i];
+                        while (true)
+                        {
+                            int C = A_temp % B_temp;
+                            if (C == 0)
+                            {
+                                Console.WriteLine("{0}/{1}",arr[0]/B_temp,arr[i]/B_temp);
+                                break;
+                            }
+                            else
+                            {
+                                A_temp = B_temp;
+                                B_temp = C;
+                            }
+
+                        }
+
+
+                    }
+                    else
+                    {
+                        int A_temp = arr[0];////arr[0]과 arr[i]의 최대공약수를 구한다
+                        int B_temp = arr[i];
+                        while (true)
+                        {
+                            int C = B_temp % A_temp;
+                            if (C == 0)
+                            {
+                                Console.WriteLine("{0}/{1}",arr[0]/A_temp,arr[i]/A_temp);
+                                break;
+                            }
+                            else
+                            {
+                                B_temp = A_temp;
+                                A_temp = C;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
