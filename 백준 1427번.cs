57502f4099c49352118a6f9e9a6d47af1b091ca8
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-12 00:12:40 +0900

    백준 1427번. 자연수가 주어지면 각자리수로 나눠서 내림차순으로 출력

---
+++ 백준 1427번.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1427번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[10];
+            int count = 0;
+            while(true)
+            {
+                if (N > 0)
+                {
+                    arr[count] = N % 10;
+                    N = N / 10;
+                    count++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Array.Sort(arr);
+            Array.Reverse(arr); ;
+            for(int i=0;i<count;i++)
+            {
+                Console.Write("{0}", arr[i]);
+            }
+
+        }
+    }
+}
