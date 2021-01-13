5585aac31fe9b0e4f762fd93cd957ba4ab752f25
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-13 23:23:24 +0900

    백준 11051번. 동적계획법을 통해 큰 수의 이항계수를 구하는 문제

---
+++ 백준 11051번.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_11051번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            int N = int.Parse(s[0]);
+            int K = int.Parse(s[1]);
+            arr[1, 0] = 1;
+            arr[1, 1] = 1;
+            Console.WriteLine(Pas(N, K));  
+        }
+        public static int[,] arr = new int[1001, 1001];
+        
+        private static int Pas(int n,int k)
+        {
+            if (n == k || k == 0)
+            {
+                arr[n, k] = 1;
+            }
+            else
+            {
+                if (arr[n, k] == 0)
+                {
+                    arr[n, k]= (Pas(n - 1, k) + Pas(n - 1, k - 1))%10007;
+                }
+            }
+            return arr[n, k];
+        }
+        
+
+    }
+}
