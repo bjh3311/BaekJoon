0d3db1ca49f62493da336cbe31386fb25edbb6f8
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-14 22:21:03 +0900

    백준 2751번. 시간복잡도가 O(nlogn)인 정렬 알고리즘으로 풀어야 하는 문제지만..
    대표적인 예시로 힙 정렬과, 병합 정렬이 있지만 어려운 알고리즘이므로 내장함수를 사용하는것을 백준에서 권장했다.
    그래서 C#의 내장함수인 Array.Sort()로 구현하였다.

---
+++ 백준 2751번.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2751번
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            StringBuilder sr = new StringBuilder();
+            for (int i = 0; i < N; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            for (int i = 0; i < N; i++)
+            {
+                sr.Append(arr[i]);
+                sr.Append("\n");
+            }
+            Console.Write(sr.ToString());
+        }
+    }
+}
