c78d1694f8c48ee0dcd7c50eb20bd3d2023b6c73
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-15 01:47:01 +0900

    백준 2108번. 정렬을 활용하는 문제.

---
+++ 백준 2108번.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_2108번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] arr = new int[N];
+            int[] count = new int[8001];//정수의 범위는 -4000~4000 , -4000은 인덱스 0에 4000은 인덱스 8000에
+            for(int i=0;i<N;i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            Array.Sort(arr);
+            double sum = 0;
+            for(int i=0;i<N;i++)
+            {
+                sum += arr[i];
+                count[arr[i] + 4000]++;
+
+            }
+            int max = -1;
+           for(int i=0;i<8001;i++)
+            {
+                if(max<count[i])
+                {
+                    max = count[i];
+                }
+            }
+            int num = 0;
+            int index = 0;
+           for(int i=0;i<8001;i++)
+            {
+                if(max==count[i])
+                {
+                    num++;
+                    if(num==2)
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            if (num == 1)
+            {
+                for(int i=0;i<8001;i++)
+                {
+                    if(max==count[i])
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+                
+                
+            Console.WriteLine(Math.Round(sum/N));
+            Console.WriteLine(arr[N / 2]);
+            Console.WriteLine(index-4000);
+            Console.WriteLine(arr[N - 1] - arr[0]);
+
+        }
+    }
+}
