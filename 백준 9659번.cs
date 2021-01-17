61cfb77e30c17179d0c11903d73f4773ce9d2522
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-17 22:36:59 +0900

    백준 9659번. 어려워보이지만 정말 단순한 문제. 왜 solved.ac에서 이걸 골드5로 했는지 의문

---
+++ 백준 9659번.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_9659번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong N = ulong.Parse(Console.ReadLine());
+            /* 1 이면 상근 
+             * 2이면 찬영 
+             * 3이면 상근
+             * 4이면 찬영 
+             * 5이면 상근
+             * 6이면 찬영
+            */
+            //걍 홀수면 상근 짝수면 찬영이다.
+            if(N%2==0)
+            {
+                Console.WriteLine("CY");
+            }
+            else
+            {
+                Console.WriteLine("SK");
+            }
+        }
+    }
+}
