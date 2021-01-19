9b552c7e132e1feb4b23e28c2989311b21f14ff0
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-20 00:15:39 +0900

    백준 9095번. 간단한 동적계획법 문제

---
+++ 백준 9095번.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_9095번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            arr[1] = 1;
+            arr[2] = 2;
+            arr[3] = 4;
+            for(int i=4;i<12;i++)
+            {
+                arr[i] = arr[i - 3] + arr[i - 2] + arr[i - 1];
+            }
+            for(int i=0;i<T;i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine(arr[a]);
+            }
+        }
+        public static int[] arr = new int[12];
+        
+    }
+}
