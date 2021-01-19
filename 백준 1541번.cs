49b5d36ccab5149be2e7f6766fe3c343fa86a5a6
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-20 01:12:44 +0900

    백준 1541번. 그리디 알고리즘 문제인데 풀이는 쉬웠으나 구현이 어려웠다.

---
+++ 백준 1541번.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_1541번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] arr = s.Split('-');
+            int result = Add(arr[0]);
+            for(int i=1;i<arr.Length;i++)
+            {
+                result -= Add(arr[i]);
+            }
+            Console.WriteLine(result);
+            
+        }
+        public static int Add(string i)
+        {
+            string[] arr = i.Split('+');
+            int result = 0;
+            for(int j=0;j<arr.Length;j++)
+            {
+                result+=int.Parse(arr[j]);
+            }
+            return result;
+        }
+    }
+}
