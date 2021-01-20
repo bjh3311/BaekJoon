c5bd274bd56f8c6e27bf96b136bf86157f834a75
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-01-21 01:07:46 +0900

    백준 13305번. 그리디 알고리즘 문제이다. 크게 어렵진 않다

---
+++ 백준 13305번.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 백준_13305번
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            ulong[] length = new ulong[N - 1];//길이들의 배열
+            ulong[] price = new ulong[N-1];//주유소 가격의 배열
+            string[] s = Console.ReadLine().Split();
+            for(int i=0;i<N-1;i++)
+            {
+                length[i] = ulong.Parse(s[i]);
+            }
+            s = Console.ReadLine().Split();
+            for(int i=0;i<N-1;i++)//마지막 주유소는 못 사용하기 때문에 N-1까지 입력
+            {
+                price[i] = ulong.Parse(s[i]);
+            }
+            ulong min_price = price[0];
+            ulong sum = (price[0] * length[0]);
+            for (int i=1;i<N-1;i++)
+            {
+                if (price[i] >=min_price)
+                {
+                    sum += min_price * length[i];
+                }
+                else if(price[i]<min_price)
+                {
+                    sum += price[i] * length[i];
+                    min_price = price[i];
+                }
+
+            }
+            Console.WriteLine(sum);
+            
+            
+        }
+    }
+}
