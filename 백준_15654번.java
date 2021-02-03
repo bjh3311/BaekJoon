9fba9b229938f6ed0bd27aaab04c22927452214d
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-02-03 16:12:51 +0900

    백준 15654번. N과M 수열 출력문제이다. 계속 전꺼를 변형해서 풀고있다.

---
+++ 백준_15654번.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 백준_15654번 {
+	public static int[] arr=new int[8];
+	public static int[] visited=new int[8];//방문했으면 1, 아니면 0
+	public static void dfs(int N,int M,int count,int[] arr_temp)
+	{
+		if(count==M)
+		{
+			for(int i=0;i<M;i++)
+			{
+				System.out.print(arr[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		for(int i=0;i<N;i++)
+		{		
+	       	if(visited[i]!=1)
+	       	{   	
+	        			
+	       		arr[count]=arr_temp[i];
+	          	visited[i]=1;
+	           	dfs(N,M,count+1,arr_temp);
+	            visited[i]=0;			
+	        }
+	    }	
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+	    int  N=scan.nextInt();
+	    int M=scan.nextInt();	
+	    int[] arr_temp=new int[N];
+	    for(int i=0;i<N;i++)
+	    {
+	    	arr_temp[i]=scan.nextInt();
+	    }
+		Arrays.sort(arr_temp);//입력받은 자연수를 오름차순으로 정렬
+	    dfs(N,M,0,arr_temp);	
+	}
+	
+}
