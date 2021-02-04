4f0d1d37b29f8eca900cac2cee42ee1fab3d4024
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-02-04 20:00:13 +0900

    백준 9660번. 간단한 게임이론문제이다.

---
+++ 백준_9660번.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class น้มุ_9660น๘ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		long N=scan.nextLong();
+		if(N%7==2||N%7==0)
+		{
+			System.out.println("CY");
+		}
+		else
+		{
+			System.out.println("SK");
+		}
+
+	}
+
+}
