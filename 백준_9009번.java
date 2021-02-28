071020477857dd9ffd9cd7ff36a4c249524cacfe
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-02-28 17:41:17 +0900

    백준 9009번. 그리디 알고리즘으로 피보나치 수열 출력문제
    처음풀었을때 출력초과의 원인을 못찾아서 엄청 헤멨는데 시간이 지나고 다시보니 걍 N값 범위를 잘못봐서 그랬다.

--- 백준_9009번.java
+++ 백준_9009번.java
@@ -1,10 +1,7 @@
-import java.util.Scanner;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
+import java.io.*;
 public class 백준_9009번 {
-    public static int[] Fibonnaci=new int[41];//피보나치수를 구하기위함
-    public static int Fib(int n)//동적계획법으로 피보나치수를 구하려고함
+    public static long[] Fibonnaci=new long[46];//피보나치수를 구하기위함
+    public static long Fib(int n)//동적계획법으로 피보나치수를 구하려고함
     {
          if(n<2)
          {
@@ -20,21 +17,21 @@ public class 백준_9009번 {
          }
     }
 	public static void main(String[] args) throws IOException {
-		Scanner scan=new Scanner(System.in);
-		int T=scan.nextInt();//테스트 케이스 갯수 입력
-		int[] num=new int[T];//주어진 정수입력
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		int T=Integer.parseInt(br.readLine());//테스트 케이스 갯수 입력
+		long[] num=new long[T];//주어진 정수입력
 		for(int i=0;i<T;i++)
 		{
-			num[i]=scan.nextInt();
+			num[i]=Long.parseLong(br.readLine());
 		}//주어진 정수 모두입력
 		Fibonnaci[0]=0;
 		Fibonnaci[1]=1;//피보나치 초기값 설정
-		int a=Fib(40);//실행함으로써 피보나치값 40까지 모두 구함
-		int[][] result=new int[T][41];//결과값 배열 선언
+		long a=Fib(45);//실행함으로써 피보나치값 40까지 모두 구함
+		long[][] result=new long[T][46];//결과값 배열 선언
 		for(int i=0;i<T;i++)
 		{
-			for(int j=40;j>0;j--)
+			for(int j=45;j>0;j--)
 			{
 				if(num[i]>Fibonnaci[j])
 				{
@@ -50,7 +47,7 @@ public class 백준_9009번 {
 		}//그리디 알고리즘으로 결과 배열에 입력
 		for(int i=0;i<T;i++)
 		{
-			for(int j=1;j<=40;j++)
+			for(int j=1;j<=45;j++)
 			{
 				if(result[i][j]!=0)
 				{
