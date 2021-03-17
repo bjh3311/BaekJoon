99a29ff5c92e743d2d5588346bff862ec5a56dc1
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-03-18 02:05:29 +0900

    백준 16678번. 그리디 알고리즘 문제이다.
    그리디 알고리즘은 이렇다할 정석풀이가 없는거 같다.

---
+++ 백준_16678번.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class น้มุ_16678น๘ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		int N=scan.nextInt();
+		int[] arr=new int[N];
+		for(int i=0;i<N;i++)
+		{
+			arr[i]=scan.nextInt();
+		}
+		Arrays.sort(arr);
+		int ans=0;
+		ans+=arr[0]-1;
+		arr[0]=1;
+		for(int i=1;i<N;i++)
+		{		
+			if(arr[i]>arr[i-1])
+			{
+				ans+=arr[i]-(arr[i-1]+1);	
+				arr[i]=arr[i-1]+1;
+			}		
+		}
+		System.out.println(ans);
+	}
+}
