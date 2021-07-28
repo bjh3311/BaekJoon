6b7213236c66e69c22557373149e2adee7b3db23
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-07-28 15:36:29 +0900

    백준 10775번. 재채점으로 시간초과 발생해서 유니온-파인드로 해결

--- 백준_10775번.java
+++ 백준_10775번.java
@@ -1,36 +1,47 @@
 import java.util.*;
 import java.io.*;
 public class 백준_10775번 {
-
+	public static int G,P;
+	public static int result=0;
+	public static int[] parents;
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		int G=Integer.parseInt(br.readLine());
-		int P=Integer.parseInt(br.readLine());
-		boolean[] avail=new boolean[G];
-		int ans=0;
-		boolean closure=true;
+		int G=Integer.parseInt(br.readLine());//게이트 개수
+		int P=Integer.parseInt(br.readLine());//비행기 개수
+		parents=new int[G+1];
+		for(int i=1;i<=G;i++)
+		{
+			parents[i]=i;
+		}
 		for(int i=0;i<P;i++)
 		{
-			int a=Integer.parseInt(br.readLine());
-			int loc=a;
-			closure=true;
-			for(int j=loc-1;j>=0;j--)
-			{
-				if(!avail[j])
-				{
-					closure=false;
-					avail[j]=true;
-					ans++;
-					break;
-				}
-			}
-			if(closure)
+			int g=Integer.parseInt(br.readLine());
+			int emptyGate=find(g);
+			if(emptyGate==0)//emptyGate가 0이라는뜻은 더이상 넣을 게이트가 없다는 뜻
 			{
 				break;
 			}
+			result++;
+			union(emptyGate,emptyGate-1);
 		}
-		System.out.println(ans);
+		System.out.println(result);
 	}
-
+	public static int find(int x)
+	{
+		if(x==parents[x])//만약 x값이 들어왔는데 parents[x]=x , 즉 x게이트에 넣을수 있으면 x게이트에 넣는다.
+		{
+			return x;
+		}
+		return parents[x]=find(parents[x]);
+	}
+	public static void union(int a,int b)
+	{
+		a=find(a);
+		b=find(b);
+		if(a!=b)
+		{
+			parents[a]=b;
+		}
+	}
 }
