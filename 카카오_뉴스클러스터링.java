0ba9b6b6a18f4fed9a2d3435f17d1246afdf7b29
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-08-27 01:19:43 +0900

    카카오 뉴스클러스터링
    문자열 문제는 너무 신경써줘야 하는게 많은 거 같다.

--- 카카오_뉴스클러스터링.java
+++ 카카오_뉴스클러스터링.java
@@ -7,22 +7,74 @@ public class 카카오_뉴스클러스터링 {
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 		String str1=br.readLine();
 		String str2=br.readLine();
-		System.out.println(solution(str1.toUpperCase(),str2.toUpperCase()));//대소문자를 구별하지 않기때문에 
-		
-
+		System.out.println(solution(str1,str2));//대소문자를 구별하지 않기때문에 
 	}
 	public static int solution(String str1,String str2)
 	{
-		HashSet<String> set=new HashSet<String>();
-		int inter=0;//교집합 크기
-		int sum=0;//합칩합 크기
-		for(int i=0;i<str1.length();i++)
+		str1=str1.toUpperCase();
+		str2=str2.toUpperCase();
+		ArrayList<String> set1=new ArrayList<String>();
+		ArrayList<String> set2=new ArrayList<String>();
+		ArrayList<String> inter=new ArrayList<String>();
+		ArrayList<String> union=new ArrayList<String>();
+		for(int i=2;i<=str1.length();i++)
 		{
+			String temp=str1.substring(i-2,i);
+			boolean proper=true;
+			for(int j=0;j<2;j++)
+			{
+				if((int)temp.charAt(j)<65||(int)temp.charAt(j)>90)
+				{
+					proper=false;
+				}
+			}
+			if(proper)
+			{
+				set1.add(temp);
+			}
+		}
+		for(int i=2;i<=str2.length();i++)
+		{
+			String temp=str2.substring(i-2,i);
+			boolean proper=true;
+			for(int j=0;j<2;j++)
+			{
+				if((int)temp.charAt(j)<65||(int)temp.charAt(j)>90)
+				{
+					proper=false;
+				}
+			}
+			if(proper)
+			{
+				set2.add(temp);
+			}
 			
 		}
-		float answer=sum/inter;
-		
+		Collections.sort(set1);
+		Collections.sort(set2);
+		for(String s:set1)
+		{
+			if(set2.remove(s))
+			{
+				inter.add(s);
+			}
+			union.add(s);
+		}
+		for(String s:set2)
+		{
+			union.add(s);
+		}
+		float answer=0;
+		if(union.size()==0)
+		{
+			answer=1;
+		}
+		else
+		{
+			answer=(float)inter.size()/(float)union.size();
+		}
 		return (int)(answer*65536);
+		
 	}
 
 }
