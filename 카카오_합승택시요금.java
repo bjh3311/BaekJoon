46f94b40025fd652b35e06558a6a8d4a8d923762
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-09-01 20:49:09 +0900

    카카오 합승택시요금
    결국 플로이드-와샬 알고리즘으로 해결했다.

--- 카카오_합승택시요금.java
+++ 카카오_합승택시요금.java
@@ -1,19 +1,5 @@
 import java.util.*;
 import java.io.*;
-class Node implements Comparable<Node>
-{
-	int v;
-	int w;
-	Node(int v,int w)
-	{
-		this.v=v;
-		this.w=w;
-	}
-	public int compareTo(Node o)
-	{
-		return w-o.w;
-	}
-}
 public class 카카오_합승택시요금 {
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
@@ -38,78 +24,50 @@ public class 카카오_합승택시요금 {
 		}
 		System.out.println(solution(n,s,a,b,fares));
 	}
-	public static ArrayList<Node>[] arr;
-	public static PriorityQueue<Node> q=new PriorityQueue<Node>();
-	public static int[] dist;//시작점 s에서 각 지점까지의 거리
+	public static int[][] map;
 	public static int result=Integer.MAX_VALUE;
 	public static int solution(int n,int s,int a,int b,int[][] fares)
 	{
-		arr=new ArrayList[n+1];
+		int answer=0;
+		map=new int[n+1][n+1];
 		for(int i=1;i<=n;i++)
 		{
-			arr[i]=new ArrayList<Node>();
+			Arrays.fill(map[i], 100000000);
+		}
+		for(int i=1;i<=n;i++)
+		{
+			for(int j=1;j<=n;j++)
+			{
+				if(i==j)
+				{
+					map[i][j]=0;
+				}
+			}
 		}
 		for(int[] each:fares)
 		{
 			int start=each[0];
 			int end=each[1];
 			int cost=each[2];
-			arr[start].add(new Node(end,cost));
-			arr[end].add(new Node(start,cost));
+			map[start][end]=cost;
+			map[end][start]=cost;
 		}
-		dist=new int[n+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[s]=0;//시작점은 0
-		q.add(new Node(s,0));
-		boolean[] visited=new boolean[n+1];
-		while(!q.isEmpty())
+		for(int i=1;i<=n;i++)
 		{
-			Node temp=q.poll();
-			if(visited[temp.v])//방문된적이 있다면
+			for(int j=1;j<=n;j++)
 			{
-				continue;
-			}
-			visited[temp.v]=true;
-			for(Node next:arr[temp.v])
-			{
-				if(dist[next.v]>dist[temp.v]+next.w)//방문된적 없고 거리가 짧다면
+				for(int k=1;k<=n;k++)
 				{
-					dist[next.v]=dist[temp.v]+next.w;
-					q.add(new Node(next.v,dist[next.v]));
+					map[j][k]=Math.min(map[j][k], map[j][i]+map[i][k]);
 				}
 			}
 		}
 		for(int i=1;i<=n;i++)
 		{
-			dijkstra(n,i,a,b);
+			int temp=map[s][i]+map[i][a]+map[i][b];
+			result=Math.min(result,temp);
 		}
-		int answer=result;
+		answer=result;
 		return answer;
-	}
-	public static void dijkstra(int n,int s,int a,int b)//시작점 n에서 다익스트라
-	{
-		int[] temp_dist=new int[n+1];
-		boolean[] visited=new boolean[n+1];
-		Arrays.fill(temp_dist, Integer.MAX_VALUE);
-		temp_dist[s]=0;
-		q.add(new Node(s,0));
-		while(!q.isEmpty())
-		{
-			Node temp=q.poll();
-			if(visited[temp.v])//방문된적이 있다면
-			{
-				continue;
-			}
-			visited[temp.v]=true;
-			for(Node next:arr[temp.v])
-			{
-				if(temp_dist[next.v]>temp_dist[temp.v]+next.w)//방문된적 없고 거리가 짧다면
-				{
-					temp_dist[next.v]=temp_dist[temp.v]+next.w;
-					q.add(new Node(next.v,dist[next.v]));
-				}
-			}
-		}
-		result=Math.min(result, dist[s]+temp_dist[a]+temp_dist[b]);
 	}
 }
