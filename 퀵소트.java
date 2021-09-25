5b7dc2219f03b34d005831dba784e84695e70ba8
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-09-25 16:33:12 +0900

    퀵소트
    아직은 최악의상황에서는 n^2의 시간복잡도를 갖는 퀵소트이다. 
    효율적으로 변경해줘야한다.

--- 퀵소트.java
+++ 퀵소트.java
@@ -1,9 +1,55 @@
-
+import java.util.*;
+import java.io.*;
 public class Äü¼ÒÆ® {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(br.readLine());
+		int[] arr=new int[N];
+		for(int i=0;i<N;i++)
+		{
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		QuickSort(arr,0,arr.length-1);
+		for(int a:arr)
+		{
+			System.out.println(a+" ");
+		}
 
+	}
+	public static void QuickSort(int[] arr,int start,int end)
+	{
+		int left=start;
+		int right=end;
+		int pivot=arr[(start+end)/2];
+		while(left<=right)
+		{
+			while(arr[left]<pivot)
+			{
+				left++;
+			}
+			while(arr[right]>pivot)
+			{
+				right--;
+			}
+			if(left<=right)
+			{
+				int temp=arr[left];
+				arr[left]=arr[right];
+				arr[right]=temp;
+				left++;
+				right--;
+			}
+		}
+		if(start<right)
+		{
+			QuickSort(arr,start,right);
+		}
+		if(end>left)
+		{
+			QuickSort(arr,left,end);
+		}
 	}
 
 }
