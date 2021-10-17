83898c2758e9854290fbbe497eeae280635c4ad5
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-10-17 23:30:36 +0900

    백준 9370번
    다익스트라 응용문제

---
+++ 백준_9370번.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class น้มุ_9370น๘ {
+
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+
+}
