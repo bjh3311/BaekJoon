bb957a22d185a187bf1806ae54d4455f10ec3b79
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-10-20 02:21:32 +0900

    TaskScheduling 알고리즘 문제

--- 알고리즘Task.java
+++ 알고리즘Task.java
@@ -15,20 +15,6 @@ class Time implements Comparable<Time>
 		return start-o.start;
 	}
 }
-class Machine implements Comparable<Machine>
-{
-	int num;//기계 번호
-	int end;//끝나는 시간
-	public Machine(int num,int end)
-	{
-		this.num=num;
-		this.end=end;
-	}
-	public int compareTo(Machine o)//끝 시간이 작은게 우선순위가 크다
-	{
-		return end-o.end;
-	}
-}
 public class 알고리즘Task {
 
 	public static void main(String[] args) throws IOException{
@@ -45,26 +31,27 @@ public class 알고리즘Task {
 			time[i]=new Time(Si,Fi);
 		}
 		Arrays.sort(time);//시작시간을 기준으로 오름차순 정렬
-		PriorityQueue<Machine> q=new PriorityQueue<Machine>();
-		int num=1;
-		q.add(new Machine(num,time[0].end));
+		ArrayList<Integer> arr=new ArrayList<Integer>();
+		arr.add(time[0].end);
 		boolean ispossible;
 		for(int i=1;i<N;i++)
 		{
 			Time temp=time[i];
-			Machine m=q.poll();
-			if(m.end<=temp.start)
+			ispossible=false;
+			for(int j=0;j<arr.size();j++)
 			{
-				q.add(new Machine(m.num,temp.end));
+				if(arr.get(j)<=temp.start)//만약 가지고 있는 machine중 시간이 새로운 업무의 시작시간보다 작거나 같다면
+				{
+					arr.set(j, temp.end);//다시 바꿔준다
+					ispossible=true;
+					break;
+				}
 			}
-			else
+			if(!ispossible)
 			{
-				q.add(m);
-				num++;
-				q.add(new Machine(num,temp.end));
+				arr.add(temp.end);
 			}
-			
 		}
-		System.out.println(num);
+		System.out.println(arr.size());
 	}
 }
