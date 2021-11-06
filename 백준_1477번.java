4b97105f90dc2808aff5ebbbf9e79899d5138e88
배진환 <76583684+bjh3311@users.noreply.github.com>
2021-11-07 01:00:55 +0900

    divide by zero 오류 해결

--- 백준_1477번.java
+++ 백준_1477번.java
@@ -1,47 +1,68 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class 백준_1477번 {
 
-	public static void main(String[] args) throws IOException{
-		// TODO Auto-generated method stub
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		String[] s=br.readLine().split(" ");
-		int N=Integer.parseInt(s[0]);//휴개소 게수
-		int M=Integer.parseInt(s[1]);//더 세우려는 휴게소 개수
-		int L=Integer.parseInt(s[2]);//고속도로의 길이
-		int[] arr=new int[N+2];
-		arr[0]=0;
-		arr[N+1]=L;
-		s=br.readLine().split(" ");
-		for(int i=1;i<=N;i++)
-		{
-			arr[i]=Integer.parseInt(s[i-1]);
-		}
-		Arrays.sort(arr);
-		int left=0;
-		int right=L;
-		for(int i=1;i<=N+1;i++)//주어진 고속도로에서 가장 간격이 큰 곳 구하기
-		{
-			int temp=arr[i]-arr[i-1];
-			right=Math.max(right, temp);
-		}
-		while(left<=right)
-		{
-			int mid=(left+right)/2;
-			int sum=0;
-			for(int i=1;i<=N+1;i++)
-			{		
-				sum+=(arr[i]-arr[i-1]-1)/mid;
-			}
-			if(sum>M)
-			{
-				left=mid+1;
-			}
-			else
-			{
-				right=mid-1;
-			}
-		}
-		System.out.println(left);
-	}
+    static int N, M, L;
+    static int[] rest, restLen;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        rest = new int[N+1];
+        restLen = new int[N+1];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            rest[i] = Integer.parseInt(st.nextToken());
+        }
+        rest[N] = L;
+        Arrays.sort(rest);
+
+        int temp = 0, max = Integer.MIN_VALUE;
+        for (int i = 0; i <= N; i++) {
+            restLen[i] = rest[i] - temp;
+            max = Math.max(restLen[i], max);
+            temp = rest[i];
+        }
+
+        System.out.println(binarySearch(max));
+        br.close();
+    }
+
+    public static int binarySearch(int max) {
+        int ans = 0;
+        int left = 1, right = max;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+
+            if (check(mid)) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+    public static boolean check(int mid) {
+        int cnt = 0;
+        for (int i : restLen) {
+            if (mid >= i) continue;
+            cnt += Math.ceil(i / (double)mid) - 1;
+        }
+
+        if (cnt <= M) return true;
+        return false;
+    }
 }
