a52769aca0754ea818d6e48eadb64bd6a35bceb9
배진환 <76583684+bjh3311@users.noreply.github.com>
2022-01-29 23:38:10 +0900

    백준 1761번
    LCA문제이다. 빠른 LCA구현이 관건이다

---
+++ 백준_1761번.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+public class น้มุ_1761น๘ {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+	}
+
+}
